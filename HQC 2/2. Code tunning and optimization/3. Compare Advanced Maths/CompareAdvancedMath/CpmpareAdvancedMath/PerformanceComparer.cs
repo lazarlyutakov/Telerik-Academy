8f9a80e9a7cbe83dc19e63917ee576ec686bc41a
lazarlyutakov <lazarlyutakov@gmail.com>
2017-04-22 16:09:02 +0300

    add task3 -> compare advanced math

---
+++ HQC 2/2. Code tunning and optimization/3. Compare Advanced Maths/CompareAdvancedMath/CpmpareAdvancedMath/PerformanceComparer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CpmpareAdvancedMath
+{
+    internal class PerformanceComparer<T, K>
+    {
+        public void CompareSquareRoot(T tValue1, K kValue1)
+        {
+            dynamic firstTypeNumb = tValue1;
+            dynamic secondTypeNumb = kValue1;
+
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            var firstResult = Math.Sqrt(firstTypeNumb);
+
+            stopwatch.Stop();
+            var interval1 = stopwatch.Elapsed;
+
+            Console.WriteLine("Time consumed for calculating the square root of the first type value --> {0}", interval1);
+
+            stopwatch.Restart();
+
+            var secondResult = Math.Sqrt((double)secondTypeNumb);
+
+            stopwatch.Stop();
+            var interval2 = stopwatch.Elapsed;
+
+            Console.WriteLine("Time consumed for calculating the square root of the second type value --> {0}", interval2);
+            Console.WriteLine("\r\n" + new string('#', 80) + "\r\n");
+        }
+
+        public void CompareNaturalLogarithm(T tValue1, K kValue1)
+        {
+            dynamic firstTypeNumb = tValue1;
+            dynamic secondTypeNumb = kValue1;
+
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            var firstResult = Math.Log(firstTypeNumb);
+
+            stopwatch.Stop();
+            var interval1 = stopwatch.Elapsed;
+
+            Console.WriteLine("Time consumed for calculating the natural logarithm of the first type value --> {0}", interval1);
+
+            stopwatch.Restart();
+
+            var secondResult = Math.Log((double)secondTypeNumb);
+
+            stopwatch.Stop();
+            var interval2 = stopwatch.Elapsed;
+
+            Console.WriteLine("Time consumed for calculating the natural logarithm of the second type value --> {0}", interval2);
+            Console.WriteLine("\r\n" + new string('#', 80) + "\r\n");
+        }
+
+        public void CompareSinus(T tValue1, K kValue1)
+        {
+            dynamic firstTypeNumb = tValue1;
+            dynamic secondTypeNumb = kValue1;
+
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            var firstResult = Math.Sin(firstTypeNumb);
+
+            stopwatch.Stop();
+            var interval1 = stopwatch.Elapsed;
+
+            Console.WriteLine("Time consumed for calculating the sinus of the first type value --> {0}", interval1);
+
+            stopwatch.Restart();
+
+            var secondResult = Math.Sin((double)secondTypeNumb);
+
+            stopwatch.Stop();
+            var interval2 = stopwatch.Elapsed;
+
+            Console.WriteLine("Time consumed for calculating the sinus of the second type value --> {0}", interval2);
+            Console.WriteLine("\r\n" + new string('#', 80) + "\r\n");
+        }
+    }
+}
---
+++ HQC 2/2. Code tunning and optimization/3. Compare Advanced Maths/CompareAdvancedMath/CpmpareAdvancedMath/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CpmpareAdvancedMath
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            float floatValue = 2.7f;
+            double doubleValue = 487555.655d;
+            decimal decimalValue = 0.2415121554512555m;
+
+            // Float vs Double
+            var floatVsDoubleComparer = new PerformanceComparer<float, double>();
+
+            Console.WriteLine("Compare float vs double :");
+            Console.WriteLine("\r\n" + new string('-', 80) + "\r\n");
+
+            floatVsDoubleComparer.CompareSquareRoot(floatValue, doubleValue);
+            floatVsDoubleComparer.CompareNaturalLogarithm(floatValue, doubleValue);
+            floatVsDoubleComparer.CompareSinus(floatValue, doubleValue);
+
+            //Float vs Decimal
+           var floatVsDecimalComparer = new PerformanceComparer<float, decimal>();
+
+            Console.WriteLine("Compare float vs decimal :");
+            Console.WriteLine("\r\n" + new string('-', 80) + "\r\n");
+
+            floatVsDecimalComparer.CompareSquareRoot(floatValue, decimalValue);
+            floatVsDecimalComparer.CompareNaturalLogarithm(floatValue, decimalValue);
+            floatVsDecimalComparer.CompareSinus(floatValue, decimalValue);
+            
+            //Double vs Decimal
+           var doubleVsDecimalComparer = new PerformanceComparer<double, decimal>();
+
+            Console.WriteLine("Compare double vs decimal :");
+            Console.WriteLine("\r\n" + new string('-', 80) + "\r\n");
+
+            doubleVsDecimalComparer.CompareSquareRoot(doubleValue, decimalValue);
+            doubleVsDecimalComparer.CompareNaturalLogarithm(doubleValue, decimalValue);
+            doubleVsDecimalComparer.CompareSinus(doubleValue, decimalValue);
+
+        }
+    }
+}
