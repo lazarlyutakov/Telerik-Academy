411473fecda346e7708f119bc20f36458f9fb8bf
lazarlyutakov <lazarlyutakov@gmail.com>
2017-04-22 14:29:19 +0300

    simple maths - adding

---
+++ HQC 2/2. Code tunning and optimization/2. Compare Sipmle Maths/CompareSimpleMaths/CompareSimpleMaths/PerformanceComparer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareSimpleMaths
+{
+    internal class PerformanceComparer<T, K>
+    {
+        //T Value1;
+        //K Value2;
+
+        //public PerformanceComparer(T value1, K value2)
+        //{
+        //    this.Value1 = value1;
+        //    this.Value2 = value2;
+        //}
+
+        public void CompareAddition(T[] value1, K[] value2)
+        {
+            dynamic firstType = value1;
+            dynamic secondType = value2;
+
+            var stopwatch = new Stopwatch();
+
+            var arrayOfFirstTypeLength = firstType.Length;
+            var arrayOfSecondTypeLength = secondType.Length;
+
+            stopwatch.Start();
+            for (var i = 0; i < arrayOfFirstTypeLength; i++)
+            {
+                var result = 0;
+                result += firstType[i];            
+            }
+            stopwatch.Stop();
+            var interval1 = stopwatch.Elapsed;
+            Console.WriteLine("Time consumed for adding of the first type values --> {0}", interval1);
+
+            stopwatch.Restart();
+            for (var i = 0; i < arrayOfSecondTypeLength; i++)
+            {
+                var result = 0;
+                result += secondType[i];
+            }
+            stopwatch.Stop();
+            var interval2 = stopwatch.Elapsed;
+            Console.WriteLine("Time consumed for adding of the second type values --> {0}", interval2);
+        }
+    }
+}
---
+++ HQC 2/2. Code tunning and optimization/2. Compare Sipmle Maths/CompareSimpleMaths/CompareSimpleMaths/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareSimpleMaths
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            var intArray = new int[10];
+            var longArray = new long[10];
+            var floatArray = new float[10];
+            var doubleArray = new double[10];
+            var decimalArray = new decimal[10];
+
+            var intLongCompare = new PerformanceComparer<int, long>();
+
+            // Int vs Long Comparison
+            intLongCompare.CompareAddition(intArray, longArray);
+
+        }
+    }
+}
