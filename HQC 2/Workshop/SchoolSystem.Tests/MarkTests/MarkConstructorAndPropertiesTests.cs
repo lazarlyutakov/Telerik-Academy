481d8a6a2feeceed6ce270c03c3e2742142c1d6f
lazarlyutakov <lazarlyutakov@gmail.com>
2017-04-29 11:42:22 +0300

    add unit tests of class Mark

---
+++ HQC 2/Workshop/SchoolSystem.Tests/MarkTests/MarkConstructorAndPropertiesTests.cs
@@ -0,0 +1,67 @@
+﻿using Moq;
+using NUnit.Framework;
+using SchoolSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem.Tests.MarkTests
+{
+    [TestFixture]
+    public class MarkConstructorAndPropertiesTests
+    {
+        [Test]
+        public void Constructor_ShouldNotThrowException_WhenValidValueOfMarkPassed()
+        {
+            // Arrange
+            float validMarkValue = 4.5f;
+
+            // Act & Assert
+            Assert.DoesNotThrow(() => new Mark(Enums.Subject.Bulgarian, validMarkValue));
+        }
+
+        [Test]
+        public void Constructor_ShouldThrowException_WhenIvalidalueOfMarkPassed()
+        {
+            // Arrange
+            float validMarkValue = 1.5f;
+
+            // Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Mark(Enums.Subject.Bulgarian, validMarkValue));
+        }
+
+        [Test]
+        public void PropertyValueGet_ShouldGetTheCorrectValue()
+        {
+            // Arrange
+            float validMark = 4.5f;
+
+            var mark = new Mark(Enums.Subject.Bulgarian, validMark);
+
+            // Act
+            float result = (float)mark.Value;
+
+            // Assert
+            Assert.AreEqual(validMark, result);
+        }
+
+        [Test]
+        public void PropertyValueSet_ShouldSetTheCorrectValue()
+        {
+            // Arrange
+            float validMark = 4.5f;
+
+            var mark = new Mark(Enums.Subject.Bulgarian, validMark);
+
+            // Act
+            mark.Value = 5.5f;
+            var result = mark.Value;
+            var expectedOutput = 5.5f;
+
+            // Assert
+            Assert.AreEqual(expectedOutput, result);
+        }
+    }
+}
