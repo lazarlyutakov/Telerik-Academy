d46a4ecee0e6fac46b37fbab177b6bb3debc4b02
lazarlyutakov <lazarlyutakov@gmail.com>
2017-05-01 14:56:46 +0300

    add Log4Net task

---
+++ HQC 2/4. Development tools/DevelopmentToolsHW/Log4Net/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+
+namespace Log4Net
+{
+    public class Log4NetHW
+    {
+        public static void Main(string[] args)
+        {
+            ILog logger = LogManager.GetLogger(typeof(Log4NetHW));
+
+            XmlConfigurator.Configure();
+
+            // go to Log4Net/bin/Debug/log.txt to see rezult
+            logger.Info(DateTime.Now);
+        }
+    }
+}
