585afedf054d5be00c43dda4e879a963a48d562a
lazarlyutakov <lazarlyutakov@gmail.com>
2017-05-01 16:51:23 +0300

    add t4 template task

---
+++ HQC 2/4. Development tools/DevelopmentToolsHW/T4template/DaysOfTheWeek.cs
@@ -0,0 +1,14 @@
+﻿
+namespace DaysOfTheWeekDemo
+{
+   class WeekDays
+   {
+     			 public string понеделник { get; set; }
+        			 public string вторник { get; set; }
+        			 public string сряда { get; set; }
+        			 public string четвъртък { get; set; }
+        			 public string петък { get; set; }
+        			 public string събота { get; set; }
+        			 public string неделя { get; set; }
+            }
+}
