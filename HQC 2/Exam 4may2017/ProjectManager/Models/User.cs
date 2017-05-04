25a51540315ca51c24d6d8e35c70de7fc962e91d
lazarlyutakov <lazarlyutakov@gmail.com>
2017-05-04 18:00:27 +0300

    fix user class

--- HQC 2/Exam 4may2017/ProjectManager/Models/User.cs
+++ HQC 2/Exam 4may2017/ProjectManager/Models/User.cs
@@ -5,25 +5,18 @@ using System.Text;
 namespace ProjectManager.Models
 {
     public class User : IUser
-    {
-        [Required]
-        private const string UserNameIsRequiredErrorMessage = "User Username is required!";
-        
-        [Required]
-        private const string UserEmailIsRequiredErrorMessage = "User Email is required!";
-
-        [EmailAddress]
-        private const string UserEmailIsNotValidErrorMessage = "User Email is not valid!";
-        
-
+    {       
         public User(string username, string email)
         {
             this.UserName = username;
             this.Email = email;
         }
 
+        [Required(ErrorMessage = "User Username is required!")]
         public string UserName { get; set; }
 
+        [Required(ErrorMessage = "User Email is required!")]
+        [EmailAddress(ErrorMessage = "User Email is not valid!")]
         public string Email { get; set; }
 
         public override string ToString()
