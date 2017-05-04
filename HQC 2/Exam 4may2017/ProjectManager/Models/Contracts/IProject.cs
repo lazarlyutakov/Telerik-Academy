db7087d19280012b594782b052dfa6f4da21cdc1
lazarlyutakov <lazarlyutakov@gmail.com>
2017-05-04 18:40:59 +0300

    refactoredProject class

--- HQC 2/Exam 4may2017/ProjectManager/Models/Contracts/IProject.cs
+++ HQC 2/Exam 4may2017/ProjectManager/Models/Contracts/IProject.cs
@@ -1,8 +1,23 @@
-﻿using System;using System.Collections.Generic;using System.Linq;using System.Text;using System.Threading.Tasks;
-namespace ProjectManager.Models
-{
-    public interface IProject    {        string Name { get; set; }        DateTime StartingDate { get; set; }        DateTime EndingDate { get; set; }        string State { get; set; }
+﻿using ProjectManager.Enumerations;
+using System;
+using System.Collections.Generic;
 
-        List<User> Users { get; set; }        List<Task> Tasks { get; set; }
+namespace ProjectManager.Models.Contracts
+{
+    public interface IProject
+    {
+        string Name { get; set; }
+
+        DateTime StartingDate { get; set; }
+
+        DateTime EndingDate { get; set; }
+
+        ProjectState State { get; set; }
+
+        IList<IUser> Users { get; set; }
+
+        IList<ITask> Tasks { get; set; }
+
+        string ToString();
     }
 }
